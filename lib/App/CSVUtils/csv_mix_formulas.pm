29e026d78fbcd10c6eddd98be44d76ca6a5735f0
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-02-24 18:18:46 +0700

    [incompatible change] Ingredient and weight fields now must match between CSV files (but still currently picked from first two fields), all extra fields will be shown in the final formula

--- lib/App/CSVUtils/csv_mix_formulas.pm
+++ lib/App/CSVUtils/csv_mix_formulas.pm
@@ -28,29 +28,31 @@ converted to its decimal form (e.g. "60%" or "60.0 %" will become 0.6).
 
 Example, mixing this CSV:
 
-    ingredient,%weight
-    water,80
-    sugar,15
-    citric acid,0.3
-    strawberry syrup,4.7
+    ingredient,%weight,extra-field1,extra-field2
+    water,80,foo,bar
+    sugar,15,foo,bar
+    citric acid,0.3,foo,bar
+    strawberry syrup,4.7,foo,bar
 
-and this (notice the different headers, which will be ignored):
+and this:
 
-    Ingredient,% of Weight
-    lemon syrup,5.75
-    citric acid,0.25
-    sugar,14
-    water,80
+    ingredient,%weight,extra-field1,extra-field2,extra-field3
+    lemon syrup,5.75,bar,baz,qux
+    citric acid,0.25,bar,baz,qux
+    sugar,14,bar,baz,qux
+    water,80,bar,baz,qux
 
-will result in (the header by default will follow the first CSV and ingredients
-will be sorted by decreasing weight then by name):
+will result in the following CSV. Note: 1) for the header, except for the first
+two fields which are the ingredient name and weight which will contain the mixed
+formula, the other fields will simply collect values from all the CSV files. 2)
+for sorting order: decreasing weight then by name.
 
-    ingredient,%weight
-    water,80
-    sugar,14.5
-    lemon syrup,2.875
-    strawberry syrup,2.35
-    citric acid,0.275
+    ingredient,%weight,extra-field1,extra-field2,extra-field3
+    water,80,foo,bar,qux
+    sugar,14.5,foor,bar,qux
+    lemon syrup,2.875,bar,baz,qux
+    strawberry syrup,2.35,foo,bar,
+    citric acid,0.275,foo,bar,qux
 
 Keywords: compositions, mixture, combine
 
@@ -77,40 +79,96 @@ _
     },
     tags => ['category:combining'],
 
+    # we modify from csv-concat
+
     reads_multiple_csv => 1,
 
-    on_input_data_row => sub {
+    before_open_input_files => sub {
         my $r = shift;
 
-        # keys we add to the stash
-        $r->{ingredients} //= {}; # key = ingredient name, val = [weight1, weight2, ...]
-        unless (defined $r->{output_fields}) {
-            $r->{output_fields} = $r->{input_fields};
-            $r->{output_fields_idx} = $r->{input_fields_idx};
+        # we add the following keys to the stash
+        $r->{all_input_fields} = [];
+        $r->{all_input_fh} = [];
+        $r->{ingredient_field} = undef;
+        $r->{weight_field} = undef;
+    },
+
+    on_input_header_row => sub {
+        my $r = shift;
+
+        # TODO: allow to customize
+        if ($r->{input_filenum} == 1) {
+            die "csv-mix-formulas: At least 2 fields are required\n" unless @{ $r->{input_fields} } >= 2;
+
+            $r->{ingredient_field} = $r->{input_fields}[0];
+            $r->{weight_field}     = $r->{input_fields}[1];
         }
 
-        my $ingredient = $r->{input_row}[0];
-        my $weight = $r->{input_row}[1];
-        if ($weight =~ /\A(.+?)\s*%\z/) { $weight = $1 * 0.01 }
-        $r->{ingredients}{ $ingredient } //= [];
-        push @{ $r->{ingredients}{ $ingredient } }, $weight;
+        # after we read the header row of each input file, we record the fields
+        # as well as the filehandle, so we can resume reading the data rows
+        # later. before printing all the rows, we collect all the fields from
+        # all files first.
+
+        push @{ $r->{all_input_fields} }, $r->{input_fields};
+        push @{ $r->{all_input_fh} }, $r->{input_fh};
+        $r->{wants_skip_file}++;
     },
 
     after_close_input_files => sub {
         my $r = shift;
 
+        # collect all output fields
+        $r->{output_fields} = [];
+        $r->{output_fields_idx} = {};
+        for my $i (0 .. $#{ $r->{all_input_fields} }) {
+            my $input_fields = $r->{all_input_fields}[$i];
+            for my $j (0 .. $#{ $input_fields }) {
+                my $field = $input_fields->[$j];
+                unless (grep {$field eq $_} @{ $r->{output_fields} }) {
+                    push @{ $r->{output_fields} }, $field;
+                    $r->{output_fields_idx}{$field} = $#{ $r->{output_fields} };
+                }
+            }
+        }
+
+        my $ingredients = {}; # key = ingredient name, { field=> ... }
+
+        # get all ingredients
+        my $csv = $r->{input_parser};
+        for my $i (0 .. $#{ $r->{all_input_fh} }) {
+            my $fh = $r->{all_input_fh}[$i];
+            my $input_fields = $r->{all_input_fields}[$i];
+            while (my $row = $csv->getline($fh)) {
+                my $ingredient = $row->[ $r->{input_fields_idx}{ $r->{ingredient_field} } ];
+                my $weight     = $row->[ $r->{input_fields_idx}{ $r->{weight_field} } ];
+                $ingredients->{$ingredient} //= {};
+                my $ingredient_row = $ingredients->{$ingredient};
+                for my $j (0 .. $#{ $input_fields }) {
+                    my $field = $input_fields->[$j];
+                    if ($field eq $r->{weight_field}) {
+                        $ingredient_row->{$field} //= [];
+                        push @{ $ingredient_row->{$field} }, $row->[$j];
+                    } else {
+                        $ingredient_row->{$field} //= $row->[$j];
+                    }
+                }
+            }
+        }
+
+        #use DD; dd $ingredients;
+
         my $num_formulas = @{ $r->{input_filenames} };
         return unless $num_formulas;
 
         # calculate the weights of the mixed formula
-        my $mixed_formula = {}; # key=ingredient name, val=weight
-        for my $ingredient (keys %{ $r->{ingredients} }) {
-            $mixed_formula->{$ingredient} = sum( @{ $r->{ingredients}{$ingredient} } ) / $num_formulas;
+        for my $ingredient (keys %{ $ingredients }) {
+            $ingredients->{$ingredient}{ $r->{weight_field} } = sum( @{ $ingredients->{$ingredient}{ $r->{weight_field} } } ) / $num_formulas;
         }
 
-        for my $ingredient (sort { ($mixed_formula->{$b} <=> $mixed_formula->{$a}) || (lc($a) cmp lc($b)) } keys %$mixed_formula) {
-            my $weight = $mixed_formula->{$ingredient};
-          FORMAT: {
+        for my $ingredient (sort { ($ingredients->{$b}{ $r->{weight_field} } <=> $ingredients->{$a}{ $r->{weight_field} }) ||
+                                       (lc($a) cmp lc($b)) } keys %$ingredients) {
+
+          FORMAT: for my $weight ($ingredients->{ $r->{weight_field} }) {
                 if ($r->{util_args}{output_percent}) {
                     $weight = ($weight * 100) . "%";
                     last FORMAT;
@@ -122,7 +180,7 @@ _
                 }
             } # FORMAT
 
-            $r->{code_print_row}->([$ingredient, $weight]);
+            $r->{code_print_row}->($ingredients->{$ingredient});
         }
     },
 );
